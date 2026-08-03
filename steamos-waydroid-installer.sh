43a2450375699e6d1bbdf2b9979bd4fee37d0656
Timbo303 <tdshepherd512@gmail.com>
2026-08-02 19:12:40 -0500

    Update steamos-waydroid-installer.sh

--- steamos-waydroid-installer.sh
+++ steamos-waydroid-installer.sh
@@ -68,18 +68,20 @@ else
 fi
 
 #Install Paru AUR Helper
-echo Installing Paru AUR Helper. This can take a while.
-echo "*** pacman install paru ***" &>> $LOGFILE
+echo Installing pikaur AUR Helper. This can take a while.
+echo "*** pacman install pikaur ***" &>> $LOGFILE
 cd $WORKING_DIR
-echo -e "$current_password\n" | sudo -S pacman -Syu ninja base-devel openssl libarchive curl gpgme libisl libmpc glib2 linux-api-headers python3 systemd-libs libxkbcommon libxcb pixman xcb-util-wm pacman libarchive fakeroot debugedit dkms glibc cmake libsysprof-capture pcre2 libffi expat libxml2 xz icu zlib wayland-protocols libliftoff libdrm libglvnd vulkan-icd-loader vulkan-radeon mesa vulkan-tools vulkan-headers libinput seatd lcms2 libdisplay-info xcb-util libxcb qt5-base xcb-util-renderutil xcb-util-errors xcb-util-cursor wlr-protocols wlr-randr wf-recorder xdg-desktop-portal-wlr udev xorg-xwayland meson colord glslang pkg-config libxau xorg-xdm xorgproto libxdmcp scdoc --noconfirm --overwrite "*" &>> $LOGFILE
-export PKG_CONFIG_PATH="/usr/lib/pkgconfig:/usr/share/pkgconfig:$PKG_CONFIG_PATH"
-echo -e "$current_password\n" | sudo -S pacman -U --noconfirm extras/pacman/paru*.zst &>> $LOGFILE 
+echo -e "$current_password\n" | sudo -S pacman -S --needed base-devel --noconfirm &>> $LOGFILE
+git clone https://aur.archlinux.org/pikaur.git
+cd pikaur
+makepkg -si --noconfirm
+
 
 if [ $? -eq 0 ]
 then
-	echo Paru successfully installed!
+	echo pikaur AUR Helper successfully installed!
 else
-	echo Error Installing Paru
+	echo Error Installing pikaur
 	cleanup_exit
 fi
 
